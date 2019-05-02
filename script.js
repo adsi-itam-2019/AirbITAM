5e01988406118bf9be9b2f8912d12b29d7e1a672
sanchezpepe <sanchezpepe97@gmail.com>
2019-05-02 02:15:22 -0500

    minor changes

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+
+// if HTML DOM Element that contains the map is found...
+if (document.getElementById('map')){
+ 
+    // Coordinates to center the map
+    var myLatlng = new google.maps.LatLng(52.525595,13.393085);
+ 
+    // Other options for the map, pretty much selfexplanatory
+    var mapOptions = {
+        zoom: 14,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    // Attach a map to the DOM Element, with the defined settings
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+}
